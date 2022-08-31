bf052393d5eb9f55ce6ff2efdfe33ad0f3f20c74
webofceco <108555136+webofceco@users.noreply.github.com>
2022-08-31 02:02:13 -0400

    Add files via upload

---
+++ 8kyu/Opposites Attract.R
@@ -0,0 +1,16 @@
+lovefunc <- function(flower1, flower2){
+          
+          
+          
+          if((flower1 %% 2) == 0 && (flower2 %% 2) != 0){
+                    
+                    print(TRUE)
+          }
+          else if((flower2 %% 2) == 0 && (flower1 %% 2) != 0){
+                    
+                    print(TRUE)  
+          }  
+          else FALSE
+          
+          
+}
