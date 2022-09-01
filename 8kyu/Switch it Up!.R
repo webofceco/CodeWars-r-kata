1f8f53079848f781196835bc9c1f462a488ee96d
webofceco <108555136+webofceco@users.noreply.github.com>
2022-09-01 17:03:27 -0400

    Add files via upload

---
+++ 8kyu/Switch it Up!.R
@@ -0,0 +1,14 @@
+switch_it_up <- function(number){
+          
+          if (number == 0){"Zero"}
+          else if (number == 1){"One"}
+          else if (number == 2){"Two"}
+          else if (number == 3){"Three"}
+          else if (number == 4){"Four"}
+          else if (number == 5){"Five"}
+          else if (number == 6){"Six"}
+          else if (number == 7){"Seven"}
+          else if (number == 8){"Eight"}
+          else if (number == 9){"Nine"}
+          
+}
