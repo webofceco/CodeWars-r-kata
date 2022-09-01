4c150b7d480cddea835fc3e37a7d245da6200fc8
webofceco <108555136+webofceco@users.noreply.github.com>
2022-09-01 17:18:44 -0400

    Add files via upload

---
+++ 8kyu/Thinkful - Logic Drill - Traffic Light.R
@@ -0,0 +1,16 @@
+update_light <- function(current){
+          
+          if (current == "green"){
+                    
+                    print("yellow")
+                    
+          }else if (current == "yellow"){
+                    
+                    print("red")
+                    
+          }else if (current == "red"){
+                    
+                    print("green")
+          }
+          
+}
