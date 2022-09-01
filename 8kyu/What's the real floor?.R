c02e8eada59225328623893f9caa541706ac736f
webofceco <108555136+webofceco@users.noreply.github.com>
2022-09-01 17:09:43 -0400

    Add files via upload

---
+++ 8kyu/What's the real floor?.R
@@ -0,0 +1,27 @@
+get_real_floor <- function(n){
+
+                  
+          if (n == 1){
+                    
+                    print(0)
+                    
+          }else if (n > 1 && n < 13 ){
+                    
+                    print(n - 1)
+                    
+          }else if (n >= 13){
+                    
+                    print(n - 2)
+                    
+          }else if (n == 0){
+                    
+                    print(0)
+                    
+          }else if (n){
+                    
+                    print(n)
+                    
+          }
+          
+          
+}
