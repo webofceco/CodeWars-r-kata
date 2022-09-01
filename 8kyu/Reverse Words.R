c513675697b09cc53f896c9f814a389dcaba5e65
webofceco <108555136+webofceco@users.noreply.github.com>
2022-09-01 14:30:13 -0400

    Add files via upload

---
+++ 8kyu/Reverse Words.R
@@ -0,0 +1,8 @@
+solution <- function(sentence){
+          
+          string_split <- strsplit(sentence, " ")[[1]]
+          
+          
+          reversed_string <- paste(rev(string_split), collapse=" ")
+          
+}
