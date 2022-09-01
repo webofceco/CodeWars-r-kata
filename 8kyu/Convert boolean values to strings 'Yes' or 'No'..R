318979ab345b8cc045a0d35713fc0cb38be604f1
webofceco <108555136+webofceco@users.noreply.github.com>
2022-09-01 17:42:05 -0400

    Add files via upload

---
+++ 8kyu/Convert boolean values to strings 'Yes' or 'No'..R
@@ -0,0 +1,11 @@
+bool_to_word <- function(bool){
+          
+          if (bool == TRUE) 
+                    
+                    print('Yes')
+          
+          else 
+                    
+                    print('No')
+          
+}
