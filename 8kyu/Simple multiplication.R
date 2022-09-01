858feaf62c9228d0862db7ad94dcc538d405d432
webofceco <108555136+webofceco@users.noreply.github.com>
2022-09-01 16:01:54 -0400

    Add files via upload

---
+++ 8kyu/Simple multiplication.R
@@ -0,0 +1,15 @@
+simple_multiplication <- function(number) {
+          
+          if (number %% 2 == 0){
+                    
+                    print(number *8)
+          }
+          
+          else if (number %% 2 != 0){
+                    
+                    print(number*9)
+                    
+          }
+          
+          
+}
