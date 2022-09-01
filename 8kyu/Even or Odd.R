81cf7520bfca901fe50a6d0a5f633a68c10a47bd
webofceco <108555136+webofceco@users.noreply.github.com>
2022-09-01 17:43:32 -0400

    Add files via upload

---
+++ 8kyu/Even or Odd.R
@@ -0,0 +1,9 @@
+even_or_odd <- function(n) {
+
+                    n <- abs(n)
+          if(n %% 2 == 0){
+                    return('Even') 
+          }else {
+                    return('Odd')
+          }
+}
